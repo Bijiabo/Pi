fd0d3feffbe34f821da9e226df26cf141354cc04
Bijiabo <hu@cafa.me>
2014-01-25 23:46:45 +0800

    LED

---
+++ test/led.js
@@ -0,0 +1,19 @@
+/**
+ * Created by root on 14-1-25.
+ */
+var gpio = require("../pi-gpio"),
+    should = require("should"),
+    fs = require("fs");
+var LED_port = 7;
+var openLED = function(){
+    gpio.write(LED_port,1,function(){
+        var close=setTimeout(function(){
+            gpio.close(LED_port,function(){
+                openLED();
+            });
+        },500);
+    });
+}
+gpio.open(7,'output',function(err){
+    openLED();
+});
