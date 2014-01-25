1d1a6de82afaf962e70833d11f3eaf16f085907d
Bijiabo <hu@cafa.me>
2014-01-26 00:06:17 +0800

    debug

--- test/led.js
+++ test/led.js
@@ -4,11 +4,11 @@
 var gpio = require("../pi-gpio"),
     fs = require("fs");
 var LED_port = 7;
-var openLED = function(){
-    gpio.write(LED_port,1,function(){
-        openLED();
-    });
-}
-gpio.open(7,'output',function(err){
-    openLED();
-});
+var LEDopen = function(){
+        gpio.open(LED_port, "output", function(err) {     // Open pin 16 for output
+            gpio.write(LED_port, 1, function() {          // Set pin 16 high (1)
+                gpio.close(LED_port,LEDopen());                     // Close pin 16
+            });
+        });
+    }
+LEDopen();
