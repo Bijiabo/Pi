197cc36e34deaea07a7fcf6a657f8b21154f8cd9
Bijiabo <hu@cafa.me>
2014-01-27 00:14:02 +0800

    test led

--- test/led.js
+++ test/led.js
@@ -3,32 +3,18 @@
  */
 var gpio = require("../pi-gpio"),
     fs = require("fs");
-var LED_port = 7;
+var port = 7;
 var LEDopen = function(){
-        gpio.open(LED_port, "output", function(err) {     // Open pin 16 for output
+        gpio.open(port, "output", function(err) {     // Open pin 16 for output
             console.log('open----------------------------');
-            gpio.write(LED_port, 1, function() {          // Set pin 16 high (1)
-                console.log('write');
-                gpio.close(LED_port);                     // Close pin 16
-                console.log('close');
-            });
+            write(port);
         });
     }
-var toggleLED = setInterval(function(){LEDopen()},500);
-
-var LED_port2 = 11;
-var LEDopen2 = function(){
-    gpio.open(LED_port2, "output", function(err) {     // Open pin 16 for output
-        console.log('open----------------------------');
-        gpio.write(LED_port2, 1, function() {          // Set pin 16 high (1)
-            console.log('write');
-            gpio.close(LED_port2,function(){KeepPUtout(LED_port2)});                     // Close pin 16
-//            console.log('close');
-        });
-    });
+var write = function(port){
+    gpio.write(port,1,function(){
+        console.log('write donw --'+new Date());
+        write(port);
+    })
 }
-var KeepPUtout = function(port){
-    console.log('keepPutout*******');
-    gpio.write(port, 1, function(){KeepPUtout(port)});
-}
-setTimeout(function(){LEDopen2()},100);
+//var toggleLED = setInterval(function(){LEDopen()},500);
+LEDopen();
